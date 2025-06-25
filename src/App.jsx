e49e57b38dbdaf310e09907d97beacde8c9e5fbf
MAILYN707 <mailyn.delgado@ucr.ac.cr>
2025-06-25 15:15:32 -0600

    config: configuracion inicial de react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 
+
+import { Home } from './pages/Home';
+
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
