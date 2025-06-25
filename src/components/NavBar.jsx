35317f662e6c49f924e02d547874874e3454687d
MAILYN707 <mailyn.delgado@ucr.ac.cr>
2025-06-25 17:14:34 -0600

    feat: agregar conexión al backend

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 export function Navbar() {
   return (
-    <nav className="bg-grayx-6 py-3 shadow-md">
-      <div className="flex justify-between items-center">
+    <nav className="bg-sky-400 py-3 shadow-md w-full">
+      <div className="w-full flex justify-between items-center px-6">
         <span className="text-white text-xl font-semibold">Biblioteca UCR</span>
         <ul className="flex space-x-6 text-white font-medium">
           <li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import logo from '../assets/Logo.jpeg';
+import logo from '../assets/Logo.png';
 
 export function Home() {
     return (
-        <div className="w-full h-screen">
-            <div className="bg-blue-500 text-white text-3xl p-10">
-                ✅ Tailwind está funcionando correctamente
+        <div className="w-full h-screen flex flex-col">
+            {/* Cuerpo gris con logo y texto */}
+            <div className="bg-gray-300 flex-1 flex items-center justify-center">
+                <div className="flex items-center">
+                    <img
+                        src={logo}
+                        alt="Logo del sistema"
+                        className="w-[120px] h-auto mr-6"
+                    />
+                    <h2 className="text-white text-2xl font-light leading-tight">
+                        SISTEMA BIBLIOTECARIO<br />UCR
+                    </h2>
+                </div>
             </div>
         </div>
     );
